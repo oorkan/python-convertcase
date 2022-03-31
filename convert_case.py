f87501c3b7f447550001dae480cfdc40b9fc1dcd
leonidkotanidi <leocotanidi@gmail.com>
2022-04-01 03:13:40 +0400

    feat(option): Adding new option for convert string by even ordering

--- convert_case.py
+++ convert_case.py
@@ -1,13 +1,14 @@
-message = input("Please write the string you want to case-convert: ")
-option = input("Please choose from the following options: \n \
+message = str(input("Please write the string you want to case-convert: "))
+option = str(input("Please choose from the following options: \n \
   L for lower case \n \
   U for upper case \n \
   T for title case \n \
   C for capitalized case \n \
   S for swap case \n \
-  ______________________________ \n \
+  OU for upper case by even ordering \n \
+______________________________ \n \
 \n \
-  > ")
+  > "))
 
 if option.lower() == "l":
 	message = message.lower()
@@ -24,4 +25,23 @@ if option.lower() == "c":
 if option.lower() == "s":
 	message = message.swapcase()
 
+if option.lower() == "ou":
+    order = 0
+    counter = 0
+    listO = []
+    for each in message:
+        if each == " ":
+            counter=0            
+        if(counter % 2 == 0):
+            listO.append(each.upper())
+        else:
+            if each != " " and message[order] == each.upper():
+                listO.append(message[order].lower())              
+            else:
+                listO.append(message[order])
+                               
+        order+=1
+        counter+=1
+    message = ''.join(listO)
+    
 print("\n" + message)
